3ba04757f21ab7765649ee7a07a9daaf2a8e8763
reisub0 <reisub0@gmail.com>
2018-08-04 00:10:16 +0530

    Added five-second-script for demonstrating job control
    Signed-off-by: reisub0 <reisub0@gmail.com>

---
+++ job-control/five-second-script.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# five-second-script.sh
+# Copyright (C) 2018 g <g@ABCL>
+#
+# Distributed under terms of the MIT license.
+#
+
+while true; do
+    sleep 5
+    echo "It has been five seconds, time is now $(date)"
+done
